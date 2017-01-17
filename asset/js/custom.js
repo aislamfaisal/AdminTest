4e7b3e275af85bdfcf8d5f87490cc0050f0c0af1
Ariful Islam <aislamfaisal@gmail.com>
2017-01-17 16:52:37 +0600

    Added button

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,1 +1,12 @@
-/* Doughnut Chart */
+$(function() {
+    $(".expand").on( "click", function() {
+        // $(this).next().slideToggle(200);
+        $expand = $(this).find(">:first-child");
+
+        if($expand.text() == "+") {
+            $expand.text("-");
+        } else {
+            $expand.text("+");
+        }
+    });
+});
